9bf3554e8dcdd7541cd5fb4e7340a6f3e8182ce2
JackOrth <jackorth19@gmail.com>
2021-09-02 13:22:25 -0600

    Another commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('web 45 rocks!')
+require('dotenv').config() // this allows to "inject"
+const PORT = process.env.PORT || 5000
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const server = express()
+
+server.use(express.json()) //teaches express to parse req.body
+server.use(cors())
+server.use(helmet())
+
+server.get("/", (req, res)=>{
+    res.send(`
+    <h1>Web 45 Is Awesome!</h1>
+    `)
+})
+
+server.use('*', (req,res)=>{
+    res.json({
+        message: "web 45 is so awesome"
+    })
+})
+
+server.listen(PORT, ()=>{ // heroku machine uses a different port
+    console.log(`listening on ${PORT}`)
+})
